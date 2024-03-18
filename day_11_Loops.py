3f64a52ce91450d2381dc34c887b80552330141c
Vikash Dangi <130730250+vikashdangi70@users.noreply.github.com>
2024-03-18 18:34:19 +0530

    Add files via upload

---
+++ day_11_Loops.py
@@ -0,0 +1,54 @@
+#print("Hii! vikash")
+
+#loop variables
+#condition expression
+#updation expression
+#body of loop
+
+
+#there are 2 type of loops: 
+#                  1.Entry control loop
+#                  2.Exit control loop   (python me exit control nahi hota) 
+#                                        (In python there is no do while loop & also switch)
+
+#SYNTaX
+
+#loop variable =0
+#while(condition_expression):
+    #body of loop
+    #updation expression
+
+#write a program to print first 10 natural no.
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i += 1
+#     print("")
+
+#range()
+
+#for loop  
+#     IMPORTANT POINT  
+#         we can directly use sq.and non sequen. datatypes in for loop 
+
+# i=1
+# for i in range (1,11,3):   #range function for loop me integers par itrate karta hai 
+#     print(i)
+
+#synax of range 
+
+# range (start ,end+1,update)
+# range(start,end +1) by default update by 1
+# range (end +1)by default start=0,update=1
+
+# for i in range(11):
+#     print(i)
+
+#ulta loop chalana hai to 
+
+# for i in range(11,1,-1):
+#     print(i)
+
+
+
---
+++ day_12_loop sunday.py
@@ -0,0 +1,52 @@
+#day 12
+#break
+#continuous
+#else
+
+# n=10
+# while n >= 1:
+#     print(n)
+#     n-=1
+
+
+#                                           -:Continue:-
+
+# n=11
+# while n>=1:
+
+#     n -=1 
+#     if n==5:
+#         continue  #skip the code below the statement and move the control towards updation exp 
+#     print(n)
+ 
+
+#                                           -:BREAK:-
+
+# n=11
+# while n>=1:
+
+#      n -=1 
+#      if n==5:
+#          break  #break the loop and take the control out of loop 
+#      print(n)
+#this is run after break
+
+
+
+# n=11
+# while n>=1:
+#     n-=1
+#     if n==5:
+#         continue
+#     print(n)
+# else:
+#     print("else ran.....")
+
+
+n=1
+while n>=1:
+    pass
+if n%2==0:
+    pass    #pass is use as a place holder when i have no statement 
+else:
+    print("hello")
